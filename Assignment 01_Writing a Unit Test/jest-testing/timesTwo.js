3bcf6615e8a0715f637541cdebbf49f252b7f191
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-07 17:44:20 +0500

    Working with Unit Test in JavaScript

---
+++ Assignment 01_Writing a Unit Test/jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(number) {
+    return number * 2;
+}
+module.exports(timesTwo);
+
+// Task 2: Export the timesTwo function as a module
